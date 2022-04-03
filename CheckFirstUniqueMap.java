d31d0897002292d1e3fbb8bf7d14a04613e89c1c
Sezin Kokum Mutludoğan <sezinkokum@gmail.com>
2022-04-03 16:18:59 +0300

    Checking first unique element in an array with O(n) - with map

--- CheckFirstUniqueMap.java
+++ CheckFirstUniqueMap.java
@@ -14,7 +14,19 @@ class CheckFirstUniqueMap{
      }
    }
 	 return -1;
- }         
+ }  
+   public static String arrayToString(int arr[]){
+    if (arr.length > 0){
+      String result = "";
+      for(int i = 0; i < arr.length; i++) {
+        result += arr[i] + " ";
+      }
+      return result;
+    }
+    else {
+      return "Empty Array!";
+    }
+  }       
  public static void  main(String args[]) {
 
     int[] arr = {2, 54, 7, 2, 6, 54};
